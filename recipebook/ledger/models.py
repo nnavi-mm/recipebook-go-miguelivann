b872abcb121ff1d729dfdd5574064d3120168931
Miggy Go <miguelivann1129@gmail.com>
2025-03-10 15:37:37 +0800

    Created accounts app, modified gitignore, and added author field to Recipe model in Ledger

--- recipebook/ledger/models.py
+++ recipebook/ledger/models.py
@@ -15,6 +15,7 @@ class Ingredient(models.Model):
 
 class Recipe(models.Model):
     name = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
 
     def __str__(self):
         return self.name
