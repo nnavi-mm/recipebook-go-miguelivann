4f14ebea723235703441a3d53ec6db6071e07a2a
Miggy Go <miguelivann1129@gmail.com>
2025-03-24 14:54:46 +0800

    Removed redundant line in settings.py for STATIC_URL

--- recipebook/recipebook/settings.py
+++ recipebook/recipebook/settings.py
@@ -119,12 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_DIRS = [
 BASE_DIR/'static',
 ]
-STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/recipes/list'
 LOGOUT_REDIRECT_URL = '/accounts/login'
 EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
